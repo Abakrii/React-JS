7c90c24465d82381a8cf4f762816bef0f7b78590
Abdelrahman Bakry <bakry@gmail.com>
2018-06-28 23:52:22 +0200

    update functions:
    1-Edit items: simply using the inputs.
    2-Delete items: clicking on the X.
    3-Add new item: simply after starting to type into the last input.
    4-The component have an onChange event handler which called with an array of the actual items excluding the last empty one when the user clicks save.
    5-The component able to be updated at any time via its props.
    6-Use PropTypes
    7-Do Some UI Styles

--- src/Bene.js
+++ src/Bene.js
@@ -1,50 +1,118 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from "prop-types";
+
+import {
+    withStyles,
+} from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from "@material-ui/core/FormControl";
+
+import IconButton from '@material-ui/core/IconButton';
+import AppBar from '@material-ui/core/AppBar';
+import classNames from 'classnames';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    margin: {
+        margin: theme.spacing.unit,
+    },
+    textField: {
+        flexBasis: 200,
+    },
+});
 
 class Bene extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      benestudio: [{ name: '' }],
-    };
-  }
-  handleBenestudioNameChange = (idx) => (evt) => {
-    const newBenestudio = this.state.benestudio.map((benestudio, sidx) => {
-      if (idx !== sidx) return benestudio;
-      return { ...benestudio, name: evt.target.value };
-    });
-    this.setState({ benestudio: newBenestudio });
-  }
+    constructor() {
+        super();
+        this.state = {
+            benestudio: [
+                {name: ''}
+            ],
+        };
+    }
 
-  handleSubmit = (evt) => {
-    const { name, benestudio } = this.state;
-    alert(`You Have Canceld: ${name}  with ${benestudio.length} benestudio Developers`);
-  }
-  handleAddBenestudio = () => {
-    this.setState({ benestudio: this.state.benestudio.concat([{ name: '' }]) });
-  }
-  handleRemoveBenestudio = (idx) => () => {
-    this.setState({ benestudio: this.state.benestudio.filter((s, sidx) => idx !== sidx) });
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h4>Test</h4>
-        {this.state.benestudio.map((benestudio, idx) => (
-          <div className="benestudio">
-            <input
-              type="text"
-               placeholder={`write ${idx + 1} `}
-              value={benestudio.name}
-              onChange={this.handleBenestudioNameChange(idx)}
-            />
-            <button type="button" onClick={this.handleRemoveBenestudio(idx)} className="small">X</button>
-          </div>
-        ))}
-        <button type="button" onClick={this.handleAddBenestudio} className="small">Save</button>
-        <button>Cancel</button>
-      </form>
-    )
-  }
+    handleBenestudioNameChange = (id, event) => {
+        const benestudio = this.state.benestudio;
+        benestudio[id] = {name: event.target.value};
+        if (benestudio.length - 1 === id) {
+            benestudio.push({name: ''});
+        }
+        this.setState({benestudio});
+    };
+    handleCancel = () => {
+        document.getElementById("create-course-form").reset();
+    };
+    handleSaveBenestudio = (event) => {
+        const arr = [];
+        this.state.benestudio.map((v) => {
+            if (v.name !== "") {
+                arr.push(v.name)
+            }
+        });
+        alert(arr);
+    };
+    handleRemoveBenestudio = (id) => () => {
+        this.setState({
+            benestudio: this.state.benestudio.filter((s, sidx) => id !== sidx)
+        });
+    };
+    render() {
+        const { classes } = this.props;
+        return (
+            <form onSubmit={this.handleCancel}
+                  id="create-course-form"
+            >
+                <h4>Test</h4>
+                {this.state.benestudio.map((benestudio, idx) => (
+                    <div className={classes.root}>
+                        <FormControl className={classNames(classes.margin, classes.textField)}>
+                            <InputLabel> test attribute</InputLabel>
+                            <Input
+                                onChange={event => this.handleBenestudioNameChange(idx, event)}
+
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        >
+                                            <i
+                                                aria-label="Toggle password visibility"
+                                                type="button"
+                                                onClick={this.handleRemoveBenestudio(idx)}
+                                            >
+                                                x
+                                            </i>
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+                    </div>
+                ))}
+                <div>
+                    <AppBar position="static" style={{backgroundColor: '#87CEFA'}}>
+                        <Toolbar>
+                            <Button color="inherit"
+                                    onClick={this.handleCancel}
+                            >Cancel</Button>
+                            <Button color="inherit"
+                                    onClick={this.handleSaveBenestudio}
+                            >Save</Button>
+                        </Toolbar>
+                    </AppBar>
+                </div>
+            </form>
+        );
+    }
 }
-export default Bene;
+Bene.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Bene);
