178da293d5f2af7ef7e81d762e0652fb6acdbc79
Abdelrahman Bakry <bakry@gmail.com>
2018-06-29 22:53:46 +0200

    - Deploy And Handle Some Functions And Responsive Design.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Bene from './Bene';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Bene />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import Bene from './Bene';
-
 import registerServiceWorker from './registerServiceWorker';
-
 ReactDOM.render(<Bene />, document.getElementById('root'));
 registerServiceWorker();
